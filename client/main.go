dfc6cf3ddccf3cba48e62dd2feb7df3410f93991
ElaineGilstrom <elainegilstrom@gmail.com>
2018-12-25 04:24:42 +0000

    Added default config value and added new function for handling config files.

--- client/main.go
+++ client/main.go
@@ -14,9 +14,15 @@ imort (
 var conn, login string
 
 func main() {
-	for i := 1; i < (len(os.Args) + 1); i += 2 {
-		if err := initHandler(os.Args[i], os.Args[i + 1], "Command line"); err != 0 {
+	if len(os.Args) <= 1 {
+		if err := handleConfig("config"); err != 0 {
 			return err
+		}
+	} else {
+		for i := 1; i < (len(os.Args) + 1); i += 2 {
+			if err := initHandler(os.Args[i], os.Args[i + 1], "Command line"); err != 0 {
+				return err
+			}
 		}
 	}
 	
@@ -32,28 +38,7 @@ func main() {
 func initHandler(arg string, val string, source string) {
 	switch arg {
 		case "-c", "--config":
-			f, err := os.Open(val)
-			if err != nil {
-				fmt.Println(err)
-				return 2
-			}
-			
-			reader := bufio.NewReader(f)
-			for l, err := reader.ReadString('\n'); err != nil; l, err := reader.ReadString('\n') {// If there is a better way to do this, please change it or tell me.
-				iErr := 0
-				if l == "" {
-					continue
-				} else {
-					s := strings.Split(l, "=")
-					if len(s) != 2 {
-						fmt.Errorf("Config not formatted properly!\nA line must include 1 and only 1 equals sign.!\nGot: %s\n", l)
-						return 2
-					} else {
-						iErr = initHandler(s[0], s[1], "Config " + val)
-					}
-				}
-				return iErr
-			}
+			return handleConfig(val)
 		case "-s", "--server":
 			m, err := regexp.MatchString("[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}:[0-9]{1,5}", val)
 			if err != nil {
@@ -72,4 +57,31 @@ func initHandler(arg string, val string, source string) {
 			fmt.Errorf("Unknown arg %s. (From %s)\n", val, source)
 			return 2
 	}
+}
+
+func handleConfig(fileName string) int {
+	f, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println(err)
+		return 2
+	}
+	
+	reader := bufio.NewReader(f)
+	for l, err := reader.ReadString('\n'); err != nil; l, err := reader.ReadString('\n') {// If there is a better way to do this, please change it or tell me.
+		iErr := 0
+		if l == "" {
+			continue
+		} else {
+			s := strings.Split(l, "=")
+			if len(s) != 2 {
+				fmt.Errorf("Config not formatted properly!\nA line must include 1 and only 1 equals sign.!\nGot: %s\n", l)
+				return 2
+			} else {
+				iErr = initHandler(s[0], s[1], "Config " + fileName)
+			}
+		}
+		return iErr
+	}
+	
+	return 0
 }
