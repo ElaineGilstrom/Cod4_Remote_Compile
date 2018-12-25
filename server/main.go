358237f31f992bb5a3251ba6aeefd563c3df4d24
ElaineGilstrom <elainegilstrom@gmail.com>
2018-12-25 10:31:52 +0000

    Fixed numerous bugs/syntax errors. Migrated to helper functions for writting to the connection.

--- server/main.go
+++ server/main.go
@@ -2,7 +2,7 @@ package main
 
 import "Cod4_Remote_Compile/share"
 
-imort (
+import (
     "net"
     "fmt"
     "log"
@@ -11,7 +11,7 @@ imort (
     "strconv"
 )
 
-var port int = 1337
+var port int64 = 1337
 
 func main() {
     for i := 1; i < len(os.Args); i++ {
@@ -19,27 +19,27 @@ func main() {
             case "--port":
                 i++
                 if i >= len(os.Args) {
-                    fmt.Errorf("No port given!\n")
-                    return 2
+                    fmt.Println("ERROR: No port given!\n")
+                    return
                 }
                 var err error
-                port, err = strconv(os.Args[i])
+                port, err = strconv.ParseInt(os.Args[i], 10, 64)
                 if err != nil {
-                    fmt.Errorf("Port bad format [%s]. Must be numeric only!\nERROR: ", os.Args[i]);
+                    fmt.Println("ERROR: Port bad format [%s]. Must be numeric only!\nERROR: ", os.Args[i]);
                     fmt.Println(err)
-                    return 2
+                    return
                 } else if port <= 0 {
-                    fmt.Errorf("Provided port %d must be greater than 0.\n", port)
-                    return 2
+                    fmt.Println("ERROR: Provided port %d must be greater than 0.\n", port)
+                    return
                 }
             default:
-                fmt.Errorf("Unknown arg %s.\n", os.Args[i])
-                return 2
+                fmt.Println("ERROR: Unknown arg %s.\n", os.Args[i])
+                return
         }
     }
     
     listen, err := net.Listen("tcp", ":" + strconv.FormatInt(port, 10))
-    if err := nil {
+    if err != nil {
         panic(err)
     }
     
@@ -53,11 +53,11 @@ func main() {
     }
 }
 
-func connHandler(conn *net.Conn) {
+func connHandler(conn net.Conn) {
     defer conn.Close()
     
     
-    b := [1]byte
+    b := make([]byte, 1)
     n, err := conn.Read(b)
     if err != nil {
         fmt.Println(err)
@@ -67,25 +67,26 @@ func connHandler(conn *net.Conn) {
         return
     }
     
-    switch b[0] {
-        case 0:
+    switch share.ByteToServerCalls(b[0]) {
+        case share.ServerPing:
             n, err = conn.Write(b)
             if err != nil {
-                fmt.Println(err)
+                log.Println(err)
                 return
             } else if n == 0 {
-                fmt.Errorf("Client sent no bytes!\n")
+                log.Println("ERROR: Client sent no bytes!\n")
                 return
             }
         default:
-            msg := append([]byte[-1], []byte(fmt.Sprintf("Unknown call %d", b[0])))
             log.Printf("Recieved unknow response (%d) from client %s.\n", conn.RemoteAddr().String(), b[0])
-            n, err = conn.Write(msg)
+            errStr := []byte(fmt.Sprintf("Unknown call %d", b[0]))
+            errLen, err := share.ConvIntToByteArr(len(errStr), 4)
             if err != nil {
-                log.Printf("Error: Unable to write error msg to client %s\n", conn.RemoteAddr().String())
-            } else {
-                log.Printf("Error: Client %s did not recieve full msg. Recieved %d, Expected %d\n", conn.RemoteAddr().String(), n, len(msg))
+                fmt.Printf("Error: Unable to format int %d.\n", len(errStr))
+                fmt.Println(err)
+                return
             }
+            _ = share.WriteMsg(conn, []byte{0xFF}, errLen, errStr)
             return
     }
 }
