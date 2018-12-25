a046fbeca882c63803cb9a3943d7cf0dbf64dbef
ElaineGilstrom <elainegilstrom@gmail.com>
2018-12-25 10:34:25 +0000

    Fixed numerous bugs/syntax errors.

--- client/main.go
+++ client/main.go
@@ -2,32 +2,38 @@ package main
 
 import "Cod4_Remote_Compile/share"
 
-imort (
+import (
     "net"
     "fmt"
     "os"
     "bufio"
     //"crypto/sha256"
-    "strconv"
+    //"strconv"
     "strings"
     "time"
+    "regexp"
 )
 
 
-var conn, login string
+var connStr, login string
 var callType share.ServerCalls
 
-func main() int {
+func main() {
     if len(os.Args) <= 1 {
         if err := handleConfig("config"); err != 0 {
-            return err
+            return
         }
     } else {
         for i := 1; i < (len(os.Args) + 1); i += 2 {
             if err := initHandler(os.Args[i], os.Args[i + 1], "Command line"); err != 0 {
-                return err
+                return
             }
         }
+    }
+    
+    if connStr == "" {
+        fmt.Println("No connection was inputed!")
+        return
     }
     
     conn, err := net.Dial("tcp", connStr)
@@ -43,32 +49,33 @@ func main() int {
             n, err := conn.Write([]byte{0})
             if err != nil {
                 fmt.Println(err)
-                return 2
+                return
             } else if n == 0 {
-                fmt.Errorf("Server read no bytes!\n")
-                return 2
+                fmt.Println("ERROR: Server read no bytes!\n")
+                return
             }
-            b = [1]byte
+            b := make([]byte, 1)
             n,err = conn.Read(b)
             if err != nil {
                 fmt.Println(err)
-                return 2
+                return
             } else if n == 0 {
-                fmt.Errorf("Server sent no bytes!\n")
-                return 2
+                fmt.Println("ERROR: Server sent no bytes!\n")
+                return
             } else if b[0] != 0 {
-                fmt.Errorf("Server did not pong! (%d != 0)\n", b[0])
-                return 2
+                fmt.Println("ERROR: Server did not pong! (%d != 0)\n", b[0])
+                return
             }
             fmt.Printf("Pong! %v\n", time.Now().Sub(start))
-            return 1
+            return
         default:
-            fmt.Errorf("%s not implemented!\n", callType.String())
+            fmt.Println("ERROR: %s not implemented!\n", callType.String())
     }
 }
 
 func initHandler(arg string, val string, source string) int {
     arg = strings.ToLower(arg)
+    var err error//Initializing here for simplicities sake
     
     switch arg {
         case "-c", "--config":
@@ -78,26 +85,26 @@ func initHandler(arg string, val string, source string) int {
             if err != nil {
                 panic(err)
             } else if !m {
-                fmt.Errorf("Server ip:port [%s] did not match format 255.255.255.255:99999! (From %s)\n", val, source)
+                fmt.Println("ERROR: Server ip:port [%s] did not match format 255.255.255.255:99999! (From %s)\n", val, source)
                 return 2
             } else {
-                conn = val
+                connStr = val
             }
             return 0
         case "-l", "--login":
             login = val
             return 0
         case "-t", "--type":
-            callType,err = share.NewServerCalls(val)
+            callType, err = share.NewServerCalls(val)
             if err != nil {
-                fmt.Errorf("Error from %s: ", source)
+                fmt.Println("ERROR: Error from %s: ", source)
                 fmt.Println(err)
                 return 2
             } else {
                 return 0
             }
         default:
-            fmt.Errorf("Unknown arg %s. (From %s)\n", val, source)
+            fmt.Println("ERROR: Unknown arg %s. (From %s)\n", val, source)
             return 2
     }
 }
@@ -110,14 +117,14 @@ func handleConfig(fileName string) int {
     }
     
     reader := bufio.NewReader(f)
-    for l, err := reader.ReadString('\n'); err != nil; l, err := reader.ReadString('\n') {// If there is a better way to do this, please change it or tell me.
+    for l, err := reader.ReadString('\n'); err != nil; l, err = reader.ReadString('\n') {// If there is a better way to do this, please change it or tell me.
         iErr := 0
         if l == "" {
             continue
         } else {
             s := strings.Split(l, "=")
             if len(s) != 2 {
-                fmt.Errorf("Config %s not formatted properly!\nA line must include 1 and only 1 equals sign.!\nGot: %s\n", fileName, l)
+                fmt.Println("ERROR: Config %s not formatted properly!\nA line must include 1 and only 1 equals sign.!\nGot: %s\n", fileName, l)
                 return 2
             } else {
                 iErr = initHandler("-" + s[0], s[1], "Config " + fileName)
