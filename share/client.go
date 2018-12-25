dad41ade9ac40ca39c49089ef94860a5ebf4cf98
ElaineGilstrom <elainegilstrom@gmail.com>
2018-12-25 05:07:38 +0000

    Added shared package to help synchronize the protocols.

---
+++ share/client.go
@@ -0,0 +1,8 @@
+package cod4_remote_compile_shared
+
+//These are the different calls that can be made to the client
+type client_calls int
+
+const (
+	unknown		client_calls = -1//This is temporary for now.
+)
---
+++ share/server.go
@@ -0,0 +1,8 @@
+package cod4_remote_compile_shared
+
+//These are the different calls that can be made to the server
+type server_calls int
+
+const (
+	unknown		server_calls = -1//This is temporary for now.
+)
