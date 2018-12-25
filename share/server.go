fffa9b927c810a89b90a21aac0c9e912b12da6c4
ElaineGilstrom <elainegilstrom@gmail.com>
2018-12-25 05:58:05 +0000

    Implemented ServerCalls enum.

--- share/server.go
+++ share/server.go
@@ -1,8 +1,57 @@
 package cod4_remote_compile_shared
 
+import (
+    "strings"
+    "errors"
+    "fmt"
+)
+
 //These are the different calls that can be made to the server
-type server_calls int
+type ServerCalls int
 
 const (
-    ping            server_calls = 0
+    ping            ServerCalls = 0
+    manifest        ServerCalls = 1
+    fileTransfer    ServerCalls = 2
+    returnBackup    ServerCalls = 3
+    ls              ServerCalls = 4
+    compile         ServerCalls = 5
+    login           ServerCalls = 6
 )
+
+
+func NewServerCalls(v string) (val ServerCalls, err error) {
+    vals := map[string]ServerCalls {
+        "ping":0,
+        "manifest":1,
+        "fileTransfer":2,
+        "returnBackup":3,
+        "ls":4,
+        "compile":5,
+        "login":6
+    }
+    
+    if _, exists := vals[v]; exists {
+        return vals[v], nil
+    } else {
+        return -1, errors.New(fmt.Sprintf("Unknown Server Call %s", v))
+    }
+}
+
+func (s ServerCalls) String() string {
+    calls := [...]string {
+        "ping",
+        "manifest",
+        "fileTransfer",
+        "returnBackup",
+        "ls",
+        "compile",
+        "login"
+    }
+    
+    if s < ping || s > login {
+        return "Unknown"
+    } else {
+        return calls[s]
+    }
+}
